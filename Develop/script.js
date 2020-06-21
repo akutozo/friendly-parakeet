f971539d935c6e81d4fc89547f22dcf375c28e58
Jacob Daughtry <akutozo@gmail.com>
2020-06-21 17:16:27 -0500

    technical updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Special characters
-const spc = "`~!%^*()+-={}|[]:;'<>?,.";
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
+const specialChar = "!$%#";
+const generateButton = document.getElementById('generate')
+generateButton.addEventListener('click', writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,6 +13,40 @@ function writePassword() {
 }
 
 function generatePassword() {
+
+  //Baseline the character count
+  var baseline = 0;
+
+  // Establishing chosen characters
+
+  var minNumbers = "";
+  var minLowCase = "";
+  var minUpCase = "";
+  var minSpc = "";
+
+  // We're going to pull random characters using fromCharCode, except special characters.
+  // We will use an array for special characters.
+  var functionArray = {
+    getNumbers: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+
+    getLowCase: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+
+    getUpCase: function() {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+
+    getSpc: function() {
+      return specialChar[Math.floor(Math.random() * specialChar.length)]
+    }
+  }
+
+  //We're going to force the user to input more than the number 8, or less than the number 128.
+  //We're going to console log each choice to ensure characters are represented accurately.
+
   var pwLength = prompt("Welcome to Magic Password. To create your magic password, please enter a password length of at least 8 and no more than 128 characters.");
   var userlength = parseInt(pwLength);
   console.log("Selected a password of " + userlength + " characters.");
@@ -30,8 +64,8 @@ function generatePassword() {
   var numbers = confirm("Are numbers required?");
   if (numbers === true){
       console.log("+ Selected numbers for password combination.")
-      // minNumbers = functionArray.getNumbers();
-      // baseline++;
+      minNumbers = functionArray.getNumbers();
+      baseline++;
     }else{
       console.log("- Numbers not selected for password combination.")
       }
@@ -39,8 +73,8 @@ function generatePassword() {
   var lowCase = confirm("Do you require lower case letters?");
   if (lowCase === true){
     console.log("+ Selected lower case letters for password combination.")
-    // minLowCase = functionArray.getLowCase();
-    // baseline++;
+    minLowCase = functionArray.getLowCase();
+    baseline++;
     }else{
     console.log("- Lower case letters not selected for password combination.")
       }
@@ -48,8 +82,8 @@ function generatePassword() {
   var upCase = confirm("Do you require upper case letters?");
   if (upCase === true){
     console.log("+ Selected upper case letters for password combination.")
-    // minUpCase = functionArray.getUpCase();
-    // baseline++;
+    minUpCase = functionArray.getUpCase();
+    baseline++;
     }else{
     console.log("- Lower case letters not selected for password combination.")
       }
@@ -57,73 +91,17 @@ function generatePassword() {
   var spc = confirm("Are special characters required?");
   if (spc === true){
     console.log("+ Selected special characters for password combination.")
-    // minSpc = functionArray.getSpc();
-    // baseline++;
+    minSpc = functionArray.getSpc();
+    baseline++;
     }else{
     console.log("- Special characters not selected for password combination.")
     }
 
-
-    //Baseline the character count
-    var baseline = 0;
-
-    // Establishing chosen characters
-
-    var minNumbers = "";
-    var minLowCase = "";
-    var minUpCase = "";
-    var minSpc = "";
-
-    //We're going to force the user to input more than the number 8, or less than the number 128.
-    //We're going to console log each choice to ensure characters are represented accurately.
-    
-    var functionArray = {
-      getNumbers: function() {
-        return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-      },
-
-      getLowCase: function() {
-        return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-      },
-
-      getUpCase: function() {
-        return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-      },
-
-      getSpc: function() {
-        return spc[Math.floor(Math.random() * spc.length)]
-      }
-    }
-
-
-
-    if (numbers === true){
-      minNumbers = functionArray.getNumbers();
-      baseline++;
-    }
-    if (lowCase === true){
-      baseline++;
-      }
-    if (upCase === true){
-      minUpCase = functionArray.getUpCase();
-      baseline++;
-    }
-
-    if (spc === true){
-      minSpc = functionArray.getSpc();
-      baseline++;
-    }
-
-  // We're going to pull random characters using fromCharCode, except special characters.
-  // We will use an array for special characters.
-
-
-
   var magicPassword = "";
 
   //Let's start looping everyone in.
-  for (let i = 0; i < userlength + - (baseline); i++) {
-    var randomLoop = Math.floor(Math.random() * 4);
+  for (let i = 0; i < (userlength - baseline); i++) {
+    var randomLoop = Math.floor(Math.random() * 10);
 
     magicPassword += randomLoop;
   }
@@ -133,11 +111,13 @@ function generatePassword() {
   magicPassword += minUpCase;
   magicPassword += minSpc;
 
+  // var magicPassword = Math.random().toString(36).slice(-8);
+
   //Presto Passwordo!
   console.log(magicPassword);
   return magicPassword;  
 
-};
+}
 
 
 // GIVEN I need a new, secure password
